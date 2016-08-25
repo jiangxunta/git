b97ff4a48a2ab5caca90feda661f6c46db8e8b88
jiangxunta <1058759803@qq.com>
2016-08-25 19:05:32 +0800

    process 内容add

--- setImmediate.js
+++ setImmediate.js
@@ -1,3 +1,10 @@
 /**
  * Created by v_jiangxiaoli on 2016/8/25.
  */
+setTimeout(function(){
+    console.log('a');
+},2000);
+// 把参数函数放在下一个事件环中执行 类似于 setTimeout 设置0
+setImmediate(function(){
+    console.log('setImmediate');
+});
